61d01a9c842a4e30b0c094985c6ef588c317149c
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-02-14 01:36:09 +0530

    add icons

--- src/utils/CategoryList.ts
+++ src/utils/CategoryList.ts
@@ -1,17 +1,17 @@
 import { z } from "zod";
 
 const CategoryListValidator = z.array(
-  z.object({ id: z.number(), name: z.string() })
+  z.object({ id: z.number(), name: z.string(), icon: z.string() })
 );
 
 export type TCategoryList = z.infer<typeof CategoryListValidator>;
 
 export const CategoryList: TCategoryList = [
-  { id: 1, name: "Produce" },
-  { id: 2, name: "Prepared foods" },
-  { id: 3, name: "Canned foods & Soups" },
-  { id: 4, name: "Bakery" },
-  { id: 5, name: "Dairy & Eggs" },
-  { id: 6, name: "Frozen" },
-  { id: 7, name: "Meat & Seafood" },
+  { id: 1, name: "Produce", icon: "/fa-solid_carrot.png" },
+  { id: 2, name: "Prepared foods", icon: "/mdi_chocolate.png" },
+  { id: 3, name: "Canned foods & Soups", icon: "/game-icons_canned-fish.png" },
+  { id: 4, name: "Bakery", icon: "/material-symbols_cake-rounded.png" },
+  { id: 5, name: "Dairy & Eggs", icon: "/jam_eggs-f.png" },
+  { id: 6, name: "Frozen", icon: "/public/ion_fast-food.png" },
+  { id: 7, name: "Meat & Seafood", icon: "🍖" },
 ];
