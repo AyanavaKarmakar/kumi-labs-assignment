651661bcfbd0fba1a2586071f47e5784ddf3753a
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-02-14 20:29:27 +0530

    add item list

---
+++ src/utils/ItemsList.ts
@@ -0,0 +1,45 @@
+import { z } from "zod";
+
+const ItemsListValidator = z.array(
+  z.object({
+    id: z.number(),
+    name: z.string(),
+    icon: z.string(),
+    cost: z.string(),
+    description: z.string(),
+    discount: z.string().optional(),
+  })
+);
+
+export type TItemsList = z.infer<typeof ItemsListValidator>;
+export const ItemsList: TItemsList = [
+  {
+    id: 1,
+    name: "Banana 1 ct",
+    icon: "/assets/product/banana.svg",
+    cost: "0.69",
+    description: "18 oz",
+  },
+  {
+    id: 2,
+    name: "Strawberries",
+    icon: "/assets/product/strawberry.svg",
+    cost: "0.69",
+    description: "1 lb",
+    discount: "0.69",
+  },
+  {
+    id: 3,
+    name: "Yogurt",
+    icon: "/assets/product/yogurt.svg",
+    cost: "0.69",
+    description: "1 lb",
+  },
+  {
+    id: 4,
+    name: "Blackberries",
+    icon: "/assets/product/blackberry.svg",
+    cost: "0.69",
+    description: "1 lb",
+  },
+];
