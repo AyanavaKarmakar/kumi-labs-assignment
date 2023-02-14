ae6a7321b26d0c5f218b033097e058ee20ed536f
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-02-14 17:41:40 +0530

    fix padding for sidebar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -5,24 +5,44 @@ import clsx from "clsx";
 
 export const TopBar = () => {
   return (
-    <div className={clsx(`${mulishFont.className}`, "block text-lg lg:hidden")}>
-      {CategoryList.map(({ id, name, icon }) => (
-        <div
-          key={id}
-          className={clsx(
-            "flex flex-col items-center justify-center gap-x-3",
-            "my-[10px] cursor-pointer py-[10px] px-3",
-            `${
-              name === "Bakery"
-                ? "rounded-xl bg-[#5DA9E9] text-white"
-                : "bg-white text-[#737D94]"
-            }`
-          )}
-        >
-          <Image src={icon} width={20} height={20} alt="category icon" />
-          {name}
-        </div>
-      ))}
+    <div
+      className={clsx(
+        "flex flex-row items-center space-x-[15px] overflow-x-auto px-7",
+        `${mulishFont.className}`
+      )}
+    >
+      {CategoryList.map(({ id, name, icon }) => {
+        /**
+         * Split the name into words
+         * and get the first word
+         * to display in the top bar
+         */
+        const words = name.split(" ");
+        const firstWord = words[0];
+
+        return (
+          <div key={id} className="flex flex-col items-center">
+            <div
+              className={clsx(
+                "cursor-pointer rounded-md p-4 shadow-md",
+                firstWord === "Produce" ? "bg-[#5DA9E9]" : "bg-[#EDEDED]"
+              )}
+            >
+              <Image src={icon} width={20} height={50} alt="category icon" />
+            </div>
+            <div
+              className={clsx(
+                "mt-[5px] text-center text-[12px] font-bold",
+                `${
+                  firstWord === "Produce" ? "text-[#5DA9E9]" : "text-[#B6BAC3]"
+                }`
+              )}
+            >
+              {firstWord}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
