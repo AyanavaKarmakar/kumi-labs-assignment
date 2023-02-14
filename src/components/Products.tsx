f541f6d6704f85ef9edd7f4d92cc9fc4df342e1d
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-02-14 22:04:33 +0530

    finish styles for products

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,10 +1,11 @@
 import { ItemsList } from "../utils/ItemsList";
 import Image from "next/image";
 import clsx from "clsx";
+import { mulishFont } from "../fonts";
 
 export const Products = () => {
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-row gap-x-2">
       {ItemsList.map(({ id, name, icon, description, cost, discount }) => (
         <div key={id} className="relative h-[220px] w-[140px] py-5">
           <Image src={icon} alt={name} height={100} width={100} />
@@ -17,15 +18,46 @@ export const Products = () => {
               width={35}
             />
           </span>
-          <div className="pl-2 pt-1">
-            <p>
-              <span className={clsx(`${discount && "line-through"}`)}>
+          <div className="space-y-1 pl-2 pt-2">
+            <p
+              className={
+                (clsx("text-[20px] font-[700] text-[#100E3A]"),
+                `${mulishFont.className}`)
+              }
+            >
+              <span
+                className={clsx(
+                  `${
+                    discount
+                      ? "text-[20px] font-[700] text-[#737D94] line-through"
+                      : "text-[20px] font-[700] text-[#100E3A]"
+                  }`
+                )}
+              >
                 ${cost}
               </span>
-              {discount && <span> ${discount}</span>}
+              {discount && (
+                <span className="text-[20px] font-[700] text-[#100E3A]">
+                  {`  $${discount}`}
+                </span>
+              )}
             </p>
-            <h2>{name}</h2>
-            <p>{description}</p>
+            <h2
+              className={clsx(
+                `${mulishFont.className}`,
+                "text-[16px] font-[600] text-[#100E3A]"
+              )}
+            >
+              {name}
+            </h2>
+            <p
+              className={clsx(
+                `${mulishFont.className}`,
+                "text-[16px] font-[700] text-[#B6BAC3]"
+              )}
+            >
+              {description}
+            </p>
           </div>
         </div>
       ))}
