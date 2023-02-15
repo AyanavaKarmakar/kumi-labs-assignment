3c585bde32c39950c2750dc0a8612d33b5e0b83e
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-02-15 16:22:31 +0530

    use redux state for sidebar and top bar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,8 +1,14 @@
 import { mulishFont } from "../fonts";
+import { type RootState } from "../redux/store";
 import { CategoryList } from "../utils/CategoryList";
 import clsx from "clsx";
+import { useSelector } from "react-redux";
 
 export const SideBar = () => {
+  const selectedCategory = useSelector(
+    (state: RootState) => state.categoryInView.value
+  );
+
   return (
     <aside
       className={clsx(
@@ -17,7 +23,7 @@ export const SideBar = () => {
           className={clsx(
             "my-[10px] cursor-pointer py-[10px] px-3",
             `${
-              name === "Bakery"
+              name === selectedCategory
                 ? "rounded-xl bg-[#5DA9E9] text-white"
                 : "bg-white text-[#737D94]"
             }`
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -2,8 +2,14 @@ import { CategoryList } from "../utils/CategoryList";
 import { mulishFont } from "../fonts";
 import Image from "next/image";
 import clsx from "clsx";
+import { useSelector } from "react-redux";
+import { type RootState } from "../redux/store";
 
 export const TopBar = () => {
+  const selectedCategory = useSelector(
+    (state: RootState) => state.categoryInView.value
+  );
+
   return (
     <div
       className={clsx(
@@ -26,12 +32,14 @@ export const TopBar = () => {
               <div
                 className={clsx(
                   "h-[48px] w-[48px] cursor-pointer rounded-md p-[12px] shadow-md",
-                  firstWord === "Dairy" ? "bg-[#5DA9E9]" : "bg-[#EDEDED]"
+                  firstWord === selectedCategory
+                    ? "bg-[#5DA9E9]"
+                    : "bg-[#EDEDED]"
                 )}
               >
                 <Image
                   priority={true}
-                  src={firstWord === "Dairy" ? activeIcon : icon}
+                  src={firstWord === selectedCategory ? activeIcon : icon}
                   alt={name}
                   height={24}
                   width={24}
@@ -42,7 +50,9 @@ export const TopBar = () => {
                 className={clsx(
                   "mt-[5px] text-center text-[12px] font-bold",
                   `${
-                    firstWord === "Dairy" ? "text-[#5DA9E9]" : "text-[#B6BAC3]"
+                    firstWord === selectedCategory
+                      ? "text-[#5DA9E9]"
+                      : "text-[#B6BAC3]"
                   }`
                 )}
               >
